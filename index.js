a650ef5f06a08b1a108b2ff2b9f02db6d5b8ffbe
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 15:51:26 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+// Q100
+// use java math object to find squareroot of 144
+var num = 144;
+console.log(Math.sqrt(num));
---
+++ index.ts
@@ -0,0 +1,4 @@
+// Q100
+// use java math object to find squareroot of 144
+const num = 144;
+console.log(Math.sqrt(num)); // answer is 12
